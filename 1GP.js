7a75a79113b4b62b5f0b0d85c503cb66ff66044f
Gail <gparsloe@umass.edu>
2016-12-26 20:06:56 -0500

    Changed the for loop to go up to 1000.

--- 1GP.js
+++ 1GP.js
@@ -12,7 +12,7 @@ Find the sum of all the multiples of 3 or 5 below 1000.
     'use strict';
     var counter = 0,
         accumulator = 0;
-    for (counter = 1; counter < 10; counter++) {
+    for (counter = 1; counter < 1000; counter++) {
         console.log("hey");
         if (counter % 3 === 0 || counter % 5 === 0) {
             accumulator = accumulator + counter;
