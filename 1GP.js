7022b2fed30b8cfa3cbd6dffffb533c99a55674e
Gail <gparsloe@umass.edu>
2016-12-26 20:27:28 -0500

    1 var per line. Removed heys.

--- 1GP.js
+++ 1GP.js
@@ -9,13 +9,11 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 (function () {
     'use strict';
     var counter = 0,
-        accumulator = 0;
+    var accumulator = 0;
     for (counter = 1; counter < 1000; counter++) {
-        console.log("hey");
         if (counter % 3 === 0 || counter % 5 === 0) {
             accumulator = accumulator + counter;
         }
-
     }
     console.log(accumulator);
 }());
